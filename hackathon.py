d461d7c7ed865beb5e060985e76ae9a34a753ca9
Natalia Marcha <nxmarcha@uchicago.edu>
2026-05-10 10:40:56 -0500

    wprk

--- hackathon.py
+++ hackathon.py
@@ -1,18 +1,11 @@
 """
-https://viewer.mapme.com/05b43849-f508-4313-b6dd-0c898b326530?categories=b6e1a08e-f839-4900-8663-6c21371614f8,ad744fb3-3024-4879-a3f4-8872baa00d49
 """
 
-
 class hacking:
-    is_accessible: bool
+    answers: list[str]
 
     def __init__():
-        is_accessible = False
-    def bryan():
-        return 0
-    def natalia():
-        return 0
-    def eugenia():
-        return 0
-    def daniel():
-        return 0
+        answers = []
+        answers.append(input("Please input your name: "))
+        answers.append(input("Do you want to join a cultural/ identity based organization?"))
+        
