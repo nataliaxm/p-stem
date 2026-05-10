9d681f0d546565ff4fd5b633941d0fab0168e97f
Natalia Marcha <nxmarcha@uchicago.edu>
2026-05-10 11:55:44 -0500

    pygame work

--- hackathon.py
+++ hackathon.py
@@ -1,22 +1,51 @@
 """
 """
 import pygame
-class hacking:
+
+import sys
+
+class Hacking:
     answers: list[str]
+    screen: pygame.Surface
+    clock: pygame.time.Clock
+    questions
 
-    def __init__():
+    def __init__(self) -> None:
+        self.answers = []
+        questions = (
+            ["Are you interested in academic RSOs?", 
+            "Are you interested in Cultural or Identity-Based RSOs?",
+            "Are you interested in Sports & Recreation?",
+            ""
+            
+            
+            ]
+        )
         pygame.init()
-        screen = pygame.display.set_mode((600, 600))
-        clock = pygame.time.Clock()
 
+        pygame.display.set_caption("RSO Finder")
+        self.screen = pygame.display.set_mode((600, 600))
+        self.clock = pygame.time.Clock()
+        self.run_event_loop()
+        
 
-        answers = []
-
+    def run_event_loop():
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     raise SystemExit
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    return 0
+        self.draw_screen()
+        self.clock.tick(24)
+
+    def draw_screen(self) -> None:
+        self.surface.fill(255, 128, 128)
+        pygame.display.update()
+
+if __name__ == "__main__":
+    Hacking()
             
 
             
