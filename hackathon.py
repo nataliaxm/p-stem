e90116e4db29a1482e5a0f6ad142c07164ae8538
Natalia Marcha <nxmarcha@uchicago.edu>
2026-05-10 10:27:46 -0500

    work

--- hackathon.py
+++ hackathon.py
@@ -1,9 +1,18 @@
+"""
+https://viewer.mapme.com/05b43849-f508-4313-b6dd-0c898b326530?categories=b6e1a08e-f839-4900-8663-6c21371614f8,ad744fb3-3024-4879-a3f4-8872baa00d49
+"""
+
+
 class hacking:
-    def bryan:
+    is_accessible: bool
+
+    def __init__():
+        is_accessible = False
+    def bryan():
         return 0
-    def natalia:
+    def natalia():
         return 0
-    def eugenia:
+    def eugenia():
         return 0
-    def daniel:
+    def daniel():
         return 0
