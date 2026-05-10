02c58180a2a326e560a87d06de84fe293e6fc588
Natalia Marcha <nxmarcha@uchicago.edu>
2026-05-10 12:45:10 -0500

    pygame

--- hackathon.py
+++ hackathon.py
@@ -1,26 +1,49 @@
 """
+pygame implementation
+
 """
 import pygame
 
 import sys
 
 class Hacking:
-    answers: list[str]
+    answers: list[bool | String]
     screen: pygame.Surface
     clock: pygame.time.Clock
-    questions
+    questions: list[str]
+    academic_questions: list[str]
 
+    yes_button = pygame.Rect(150, 250, 100, 50)
+    no_button = pygame.Rect(350, 250, 100, 50)
     def __init__(self) -> None:
         self.answers = []
         questions = (
             ["Are you interested in academic RSOs?", 
             "Are you interested in Cultural or Identity-Based RSOs?",
             "Are you interested in Sports & Recreation?",
-            ""
-            
-            
+            "Are you interested in Art, Media, or Entertainment?",
+            "Are you interested in Community Service or Activism?",
+            "Are you interested in Student Government?",
+            "Are you interested in Spiritual Groups?"
             ]
         )
+        academic_questions = (
+            ["What is your major affiliation?",
+            "Are you Pre-Med?",
+            "Are you Pre-Law?",
+            "Are you interested in tech?"
+            ]
+        )
+        cultural_questions = (
+            ["What is your cultural affiliation?",
+            "Do you have another cultural affiliation?",
+            "Do you speak/want to speak any languages?"
+            ]
+        )
+        sports_questions = (
+            ["Are you interested in any of these sports?"]
+        )
+
         pygame.init()
 
         pygame.display.set_caption("RSO Finder")
@@ -35,8 +58,13 @@ class Hacking:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     raise SystemExit
-                elif event.type == pygame.MOUSEBUTTONUP:
-                    return 0
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    print(f"Mouse clicked at {event.pos}")
+                    if yes_button.collidepoint(event.pos):
+                        answers.append(True)
+                    elif no_button.collidepoint(event.pos):
+                        answers.append(False)
+                    
         self.draw_screen()
         self.clock.tick(24)
 
